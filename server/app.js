3b10acc7af9bcc0c20d3eb585ff12cd6ce48ec45
ravi352-aug <raviazad352@gmail.com>
2026-05-28 07:32:22 +0530

    Production deployment ready

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://YOUR-FRONTEND.vercel.app",
+      "https://orbitra-assignment.vercel.app",
     ],
     credentials: true,
   })
